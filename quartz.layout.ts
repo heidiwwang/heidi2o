2e1e454adf44875942512604f5bca7ab91861a73
heidiwwang <unbalancebarracudacurling@proton.me>
2026-01-09 22:01:35 -0500

    Quartz sync: Jan 9, 2026, 10:01 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,6 +32,18 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
     explorer,
   ],
   right: [
