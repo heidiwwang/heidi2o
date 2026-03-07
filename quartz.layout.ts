7b5253831d71b1537320046bba8b79d1c19906a3
heidiwwang <unbalancebarracudacurling@proton.me>
2026-03-07 15:32:49 -0500

    Quartz sync: Mar 7, 2026, 3:32 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -64,19 +64,20 @@ export const defaultContentPageLayout: PageLayout = {
     explorer,
   ],
   right: [
-    Component.Graph({
+    Component.ConditionalRender({
+      component: Component.Graph({
   localGraph: {
     drag: true, // whether to allow panning the view around
     zoom: true, // whether to allow zooming in and out
-    depth: 2, // how many hops of notes to display
-    scale: 1.5, // default view scale
+    depth: 1, // how many hops of notes to display
+    scale: 1.1, // default view scale
     repelForce: 0.5, // how much nodes should repel each other
     centerForce: 0.3, // how much force to use when trying to center the nodes
-    linkDistance: 25, // how long should the links be by default?
-    fontSize: 0.8, // what size should the node labels be?
+    linkDistance: 30, // how long should the links be by default?
+    fontSize: 0.6, // what size should the node labels be?
     opacityScale: 1, // how quickly do we fade out the labels when zooming out?
     removeTags: [], // what tags to remove from the graph
-    showTags: false, // whether to show tags in the graph
+    showTags: true, // whether to show tags in the graph
     enableRadial: false, // whether to constrain the graph, similar to Obsidian
   },
   globalGraph: {
@@ -94,6 +95,8 @@ export const defaultContentPageLayout: PageLayout = {
     enableRadial: true, // whether to constrain the graph, similar to Obsidian
   },
 }),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
